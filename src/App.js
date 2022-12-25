7c0be36d83c933613e311cfdbe9460f52ef8b8ce
student <abbosr180@gmail.com>
2022-12-25 13:51:10 +0500

    Add my react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Kal from './components/Kal'
+import s from './App.css';
+
+function App() {
+  return (
+    <div className={s.app}>
+        <Kal/>
+    </div>
+  );
+}
+
+export default App;
